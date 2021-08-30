5a8d9d522654f031813bb48ae332eb8d7516a660
XuanVu318 <89778302+XuanVu318@users.noreply.github.com>
2021-08-30 17:43:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     introTitle: 'NgocBi à!',
-    introDesc: `Cảm ơn bờ biển Hải Tiến đã 
-    sản sinh ra một viên ngọc quý hiếm
-    Ngày hôm trước là sinh nhật lãnh tụ vĩ đại
-    Ngày hôm nay sinh nhật nóc nhà vĩ đại <3 <3
+    introDesc: 'Cảm ơn bờ biển Hải Tiến đã 
+    'sản sinh ra một viên ngọc quý hiếm
+    'Ngày hôm trước là sinh nhật lãnh tụ vĩ đại
+    'Ngày hôm nay sinh nhật nóc nhà vĩ đại <3 <3'
     btnIntro: 'hihi',
     title: 'Nhân dịp sinh nhật tuổi mới, có một món quà gửi đến ngừi iu 😙',
     desc: 'Hãy lựa chọn món quà mà mình thích ',
@@ -11,8 +11,8 @@ const CONFIG = {
     btnNo: 'Một số tiền lớn :3',
     question:'Đùa chút thui hãy nói ra mong ước tuổi mới của mình nào',
     btnReply: 'Gửi cho anh <3',
-    reply: 
-    mess: 'Uki' anh sẽ cố gắng lắm hết trong khả năng của mình ahihi,
+    reply:
+    mess: 'Uki' 'anh sẽ cố gắng lắm hết trong khả năng của mình ahihi',
     messDesc: 'Xong xuôi dịch dã hãy đi ăn 1 bữa thật ngon để bù thôi nàooo.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100000037980849'
